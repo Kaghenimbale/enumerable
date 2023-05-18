c750cd19a3400ff1f3de958dacbee3ec8f4bb5f9
Kagheni <Kaghenimbale@gmail.com>
2023-05-18 15:46:51 +0200

    Add any and all methods

--- enumerable.rb
+++ enumerable.rb
@@ -7,9 +7,6 @@ class Mylist
     include Myenumerable
 
     def each(&b)
-        # for e in @list
-        #     yield e
-        # end
         @list.each(&b)
     end
 end
@@ -19,8 +16,8 @@ mylist = Mylist.new([1,2,3,4])
 # puts mylist.all? {|e| e > 5 }
 
 puts mylist.all? {|e| e < 5}
-# puts mylist.all? {|e| e > 5}
-# puts mylist.any? {|e| e == 2}
-# puts mylist.any? {|e| e == 5}
+puts mylist.all? {|e| e > 5}
+puts mylist.any? {|e| e == 2}
+puts mylist.any? {|e| e == 5}
 # puts mylist.filter {|e| e.even?}
 
--- myenumerable.rb
+++ myenumerable.rb
@@ -2,7 +2,14 @@
 
 module Myenumerable
     def all?
-        each {|e| e < 5}
+        each {|e| return false unless yield(e)}
+        true
+    end
+    def any?
+        each {|e| return true if yield(e)}
+        false
+    end
+    def filter
     end
 end
 
