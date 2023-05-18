fe2abfe6595133dcb2759c58455c20c087444cc7
Imran Hossain <blur-imran@hotmail.com>
2023-05-18 19:16:32 +0600

    Update myenumerable.rb

--- myenumerable.rb
+++ myenumerable.rb
@@ -1,9 +1,10 @@
-
-
 module Myenumerable
-    def all?
-        each {|e| e < 5}
-    end
+	def for_any?(value)
+		return value.any?{|e| yield(e)}
+	end
+    def for_any?(value)
+			return value.any?{|e| yield(e)}
+		end
 end
 
 # [1,3,5].my_all? { |e| puts e}
