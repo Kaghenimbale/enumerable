f0c75acda09b4fcc9128d7e72088747d82f3de15
Imran Hossain <blur-imran@hotmail.com>
2023-05-18 20:35:55 +0600

    Update enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -1,26 +1,23 @@
-require_relative "myenumerable.rb"
+require_relative 'myenumerable'
 
 class Mylist
-    def initialize(list)
-        @list = list
-    end
-    include Myenumerable
+  def initialize(list)
+    @list = list
+  end
+  include Myenumerable
 
-    def each(&b)
-        # for e in @list
-        #     yield e
-        # end
-        @list.each(&b)
-    end
+  def each(&bee)
+    # for e in @list
+    #     yield e
+    # end
+    @list.each(&bee)
+  end
 end
 
-mylist = Mylist.new([1,2,3,4])
+mylist = Mylist.new([1, 2, 3, 4])
 
-# puts mylist.all? {|e| e > 5 }
-
-puts mylist.for_any?{|e| e < 5}
-# puts mylist.all? {|e| e > 5}
-# puts mylist.any? {|e| e == 2}
-# puts mylist.any? {|e| e == 5}
-# puts mylist.filter {|e| e.even?}
-
+puts(mylist.any? { |e| e < 5 })
+puts(mylist.all? { |e| e > 5 })
+puts(mylist.any? { |e| e == 2 })
+puts(mylist.any? { |e| e == 5 })
+puts mylist.filter?(&:even?).inspect
